438c04996806d329b6ab2a0a9ba9cbbf6b4d8950
BCullen2201 <bjacullen01@proton.me>
2026-05-03 16:08:29 -0400

    Upload files

---
+++ scripts/dropdown.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    var list = document.getElementById("dropDownList");
+    if (list.style.display === "block") {
+        list.style.display = "none";
+    } else {
+        list.style.display = "block";
+    }
+}
