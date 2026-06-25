025a8656b03f6fba3459fcee0fb8b8175ddc7c65
BCullen2201 <bjacullen01@proton.me>
2026-06-25 13:46:19 -0400

    Works

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -4,5 +4,14 @@ function toggleMenu() {
         list.style.display = "none";
     } else {
         list.style.display = "block";
+
+        // Display link text, including count of pages
+        // When you add a page to a directory, increment the number here, instead of in the HTML
+
+        // Drop down link vars
+        var familyTreesLink = document.getElementById("FamilyTrees");
+
+        // Text to display
+        familyTreesLink.innerHTML = "Main Page: Cullen Genealogy (TEST pages)";
     }
 }
