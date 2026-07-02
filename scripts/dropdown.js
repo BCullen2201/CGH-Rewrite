32f38b814382fb8d7590a95fbe888538f09de07c
BCullen2201 <bjacullen01@proton.me>
2026-07-02 13:05:39 -0400

    Declare vars OUTSIDE of if else statement

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -1,5 +1,18 @@
 function toggleMenu() {
+    // Drop down list (The actual menu that pops up when you click the menu button)
     var list = document.getElementById("dropDownList");
+
+    // Drop down link vars
+    var whatsNewLink = document.getElementById("WhatsNew");
+    var familyTreesLink = document.getElementById("FamilyTrees");
+    var jameFamLink = document.getElementById("JameFam");
+    var novaScotLink = document.getElementById("NovaScot");
+    var rudeForefathersLink = document.getElementById("RudeForefathers");
+    var MHCullenLink = document.getElementById("MHCullen");
+    var cardCullLink = document.getElementById("CardCull");
+    var relFamLink = document.getElementById("RelFam");
+    var recordsArchiveLink = document.getElementById("RecordsArchive");
+
     if (list.style.display === "block") {
         list.style.display = "none";
     } else {
@@ -7,17 +20,6 @@ function toggleMenu() {
 
         // Display link text, including count of pages
         // When you add a page to a directory, increment the number here, instead of in the HTML
-
-        // Drop down link vars
-        var whatsNewLink = document.getElementById("WhatsNew");
-        var familyTreesLink = document.getElementById("FamilyTrees");
-        var jameFamLink = document.getElementById("JameFam");
-        var novaScotLink = document.getElementById("NovaScot");
-        var rudeForefathersLink = document.getElementById("RudeForefathers");
-        var MHCullenLink = document.getElementById("MHCullen");
-        var cardCullLink = document.getElementById("CardCull");
-        var relFamLink = document.getElementById("RelFam");
-        var recordsArchiveLink = document.getElementById("RecordsArchive");
 
         // Text to display ( ! CHANGE TEXT HERE ! )
         whatsNewLink.innerHTML = "DD/MM/YYYY";
